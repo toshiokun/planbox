eddaedabb14e7070f65bac4282889ff49680389c
Daichi0119110 <daichi0119110@gmail.com>
2015-08-22 17:33:51 +0900

    sql文が完成

--- function.php
+++ function.php
@@ -54,7 +54,7 @@ function getfavcourse($a) {
 //思い出
 function getmemories($a) {
   $dbh = connectDb();
-  $sql = "select id from couples where male_id = ".$a." or  female_id = ".$a.;
+  $sql = "select id from couples where male_id = ".$a." or  female_id = ".$a;
   $stmt = $dbh->query($sql);
   $stmt->execute;
   $couple_id = $stmt->fetch();
@@ -68,7 +68,7 @@ function getmemories($a) {
 //Feed
 function getfeeds($a) {
   $dbh = connectDb();
-  $sql = "select followed_id from follows where follow_id = ".$a.;
+  $sql = "select followed_id from follows where follow_id = ".$a;
   $stmt = $dbh->query($sql);
   $stmt->execute;
   $followed_ids = $stmt->fetch();
