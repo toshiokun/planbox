75258b627578451bb7b96a5022bcba2b339e8fb1
Toshihiro Taniguchi <10401040.taniyama@gmail.com>
2015-08-22 20:54:34 +0900

    config.phpを作成

---
+++ php/config.php
@@ -0,0 +1,15 @@
+<?php
+
+//データベース関連
+define('DSN', 'mysql:host=localhost;dbname=planbox');
+define('DB_USER', 'dbuser');
+define('DB_PASSWORD', 'planbox');
+
+//その他
+define('SITE_URL', 'http://localhost/planbox/');
+define('PASSWORD_KEY', 'planbox2015');
+
+error_reporting(E_ALL & ~E_NOTICE);
+ini_set( 'display_errors', 1 );
+
+session_set_cookie_params(0, '/planbox/');
