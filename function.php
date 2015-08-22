3ec780d5cdbbb69b41017567030c349660856e12
Daichi0119110 <daichi0119110@gmail.com>
2015-08-22 15:06:59 +0900

    database.sql function.php congig.php

---
+++ function.php
@@ -0,0 +1,63 @@
+<?php
+//use Facebook\FacebookSession;
+//use Facebook\FacebookRequest;
+//use Facebook\GraphUser;
+//use Facebook\FacebookRequestException;
+//use Facebook\FacebookJavaScriptLoginHelper;
+
+function connectDb(){
+	try {
+		return new PDO(DSN, DB_USER, DB_PASSWORD);
+	} catch (PDOException $e){
+		echo $e->getMessage();
+		exit;
+	}
+}
+
+function h($s) {
+	return htmlspecialchars($s, ENT_QUOTES, "UTF-8");
+}
+
+function setToken() {
+	$token = sha1(uniqid(mt_rand(), true));
+	$_SESSION['token'] = $token;
+}
+
+function checkToken() {
+	if (empty($_SESSION['token']) || ($_SESSION['token'] != $_POST['token'])) {
+		echo "不正な処理が行われました。";
+		exit;
+	}
+}
+
+function emailExist($email, $dbh){
+	$sql = "select * from users where email = :email limit 1";
+	$stmt = $dbh->prepare($sql);
+	$stmt->execute(array(":email" => $email));
+	$user = $stmt->fetch();
+	return $user ? true : false;
+}
+
+function userExist($facebook_id, $dbh){
+	$sql = "select * from users where facebook_id = :facebook_id limit 1";
+	$stmt = $dbh->prepare($sql);
+	$stmt->execute(array(":facebook_id" => $facebook_id));
+	$user = $stmt->fetch();
+	return $user ? true : false;
+}
+
+
+
+function getSha1Password($s){
+	return (sha1(PASSWORD_KEY.$s));
+}
+
+function getuser($a) {
+  $dbh = connectDb();
+  $sql = "select * from users where id = ".$a;
+  $stmt = $dbh->query($sql);
+  $stmt->execute;
+  $user = $stmt->fetch();
+  return $user;
+}
+
