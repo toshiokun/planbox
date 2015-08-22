fc57b4732bce9b57e5c8813b78f1f9fcb2f1b48d
Toshihiro Taniguchi <10401040.taniyama@gmail.com>
2015-08-22 18:38:02 +0900

    fav.phpを作成

--- php/fav.php
+++ php/fav.php
@@ -1,3 +1,21 @@
 <?php
+require_once('config.php');
+require_once('function.php');
 
-?>
+$fav_flg = $_POST['fav_flg'];
+$user_id = $_POST['user_id'];
+$date_id = $_POST['date_id'];
+
+$dbh = connectDb();
+$sql = "select * from favorites where user_id = ".$fav_flg." and date_id = ".$user_id;
+$stmt = $dbh->query($sql);
+$stmt->execute;
+$favorite = $stmt->fetch();
+
+if (empty($favorite)) {
+	$sql = "insert into favorites (fav_flg, user_id ,date_id created, modified) values (".!$fav_flg", ".$user_id", ".$date_id", ".time()", ".time();
+	$stmt = $dbh->query($sql);
+	$stmt->execute;
+} else {
+
+}
