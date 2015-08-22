fc6f5fd5c8aab8cd84713af48f4ccdeff2baf725
Daichi0119110 <daichi0119110@gmail.com>
2015-08-22 16:19:25 +0900

    ひな形を作成

--- index.php
+++ index.php
@@ -5,13 +5,6 @@ require_once('function.php');
 //データベースに接続
 $dbh = connectDb();
 
-//universityの情報を取得
-$universities = array();
-$sql = "select * from universities";
-foreach($dbh->query($sql) as $row){
-    array_push($universities,$row);
-}
-
 ?>
 
 <!DOCTYPE html>
