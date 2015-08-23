45fa8538fb992ab2976f7e8695c4320fd6ab15f0
Toshihiro Taniguchi <10401040.taniyama@gmail.com>
2015-08-23 13:50:37 +0900

    fav機能実装

--- php/function.php
+++ php/function.php
@@ -192,3 +192,27 @@ function setPhoto($post_id, $photo_url){
   $stmt = $dbh->query($sql);
   $stmt->execute; 
 }
+
+function setFavorite($user_id, $date_id){
+  $dbh = connectDb();
+  $sql = "select * from favorites where user_id = ".$user_id." and date_id = ".$date_id;
+  $stmt = $dbh->query($sql);
+  $stmt->execute;
+  $favorite = $stmt->fetch();
+  if (empty($favorite)) {
+    $sql = "insert into favorites (fav_flg, user_id,  date_id, created, modified) values (1, ".$user_id.", ".$date_id.", NOW(), NOW())";
+    $stmt = $dbh->query($sql);
+    $stmt->execute;
+  } else {
+    if ($favorite["fav_flg"] == 1){
+      $sql = "update favorites set fav_flg = 0 where user_id = ".$user_id." and date_id = ".$date_id;;
+      $stmt = $dbh->query($sql);
+      $stmt->execute;
+    } else {
+      $sql = "update favorites set fav_flg = 1 where user_id = ".$user_id." and date_id = ".$date_id;;
+      $stmt = $dbh->query($sql);
+      $stmt->execute;
+
+    }
+  }
+}
