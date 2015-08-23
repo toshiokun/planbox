23c9a73e00b6c693ae9f889b30c32967854ef6b8
Daichi0119110 <daichi0119110@gmail.com>
2015-08-23 15:51:09 +0900

    a

--- lp.php
+++ lp.php
@@ -46,7 +46,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand page-scroll" href="#page-top">Planbox</a>
+                <a class="navbar-brand page-scroll" href="index.php">Planbox</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
